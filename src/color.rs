eb54de4482e3263e358031dc31ed63c180e078b4
RayhaanT <tanweer.rayhaan@gmail.com>
2023-06-27 21:33:11 -0400

    Gamma correction

--- src/color.rs
+++ src/color.rs
@@ -4,14 +4,11 @@ use std::io::{BufWriter, Write};
 use crate::vec3::Color;
 
 pub fn write_color<T: Write>(out: &mut BufWriter<T>, color: &Color, samples_per_pixel: u32) {
-    let ir: u32 = (color.x * 255.99) as u32;
-    let ig: u32 = (color.y * 255.99) as u32;
-    let ib: u32 = (color.z * 255.99) as u32;
-
     let scale = 1.0 / samples_per_pixel as f64;
-    let r = color.x * scale;
-    let g = color.y * scale;
-    let b = color.z * scale;
+    // sqrt is for gamma correction with gamma = 2
+    let r = (color.x * scale).sqrt();
+    let g = (color.y * scale).sqrt();
+    let b = (color.z * scale).sqrt();
 
     let ir: u32 = (clamp(r, 0.0, 0.999) * 256.0) as u32;
     let ig: u32 = (clamp(g, 0.0, 0.999) * 256.0) as u32;
